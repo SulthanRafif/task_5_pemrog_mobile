3f5cf865c76f24cf5afdbe6a7dc1e4e6c16dec9b
SulthanRafif <rsulthan5@gmail.com>
2022-08-25 13:24:11 +0700

    create architecture

--- app/src/main/java/com/example/task_5_pemrog_mobile/ui/NewsActivity.kt
+++ app/src/main/java/com/example/task_5_pemrog_mobile/ui/NewsActivity.kt
@@ -1,11 +1,12 @@
-package com.example.task_5_pemrog_mobile
+package com.example.task_5_pemrog_mobile.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.task_5_pemrog_mobile.R
 
-class MainActivity : AppCompatActivity() {
+class NewsActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_news)
     }
 }
